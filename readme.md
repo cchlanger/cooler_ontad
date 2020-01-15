cc40b106e4e07913f62a43708e208ddbd4a799e3
Langer,Christoph <christoph.langer@imba.oeaw.ac.at>
2020-01-15 18:16:05 +0100

    Add tempfiles and fix a lot

--- readme.md
+++ readme.md
@@ -1,13 +1,10 @@
 OnTAD repo:
 https://github.com/anlin00007/OnTAD
 
-Create Input with:
-python create_dense_matrix.py testdata/G2.fc_1_2.wOldG2.cis.1000.mcool -b 50000
-
 Use our docker:
 singularity run docker://gerlichlab/mmhic:release-1.0
 
-Run there tool with following parameters:
-OnTAD tmpdata/G2.fc_1_2.wOldG2.cis.1000.chr1.matrix -penalty 0.1 -maxsz 200 -o OnTAD_cis_chr1
+Run example:
+python use_OnTAD.py testdata/G2.fc_1_2.wOldG2.cis.1000.mcool
 
 
--- use_OnTAD.py
+++ use_OnTAD.py
@@ -2,6 +2,7 @@
 import click
 import os
 import cooler
+import tempfile
 # import cooltools
 # import cooltools.snipping
 import bioframe
@@ -10,73 +11,62 @@ import numpy as np
 import pandas as pd
 
 import subprocess
-import logging
-import shlex
+# import logging
+# import shlex
 import glob
-
-
-# set wd
-os.chdir(".")
+import re
+import shutil
 
 # get chromosomal arms
 
 chromsizes = bioframe.fetch_chromsizes("hg19")
 arms = HT.getArmsHg19()
 
-# load in data
 
-# filep = "testdata/G2.fc_1_2.wOldG2.cis.1000.mcool"
-# binsize = 50000
-
-
-def convert_to_bedpe(cooler_filename, binsize, tad_filename):
+def convert_to_bedpe(cooler_filename, binsize, tad_folder):
     # get bins from cooler file
     binsize = 50000
     cooler_obj = cooler.Cooler(f'{cooler_filename}::/resolutions/{binsize}')
     bins = cooler_obj.bins()[:]
+    f_list = glob.glob('%s/*.tad' % tad_folder)
     # pixels = cooler_obj.pixels()[:]
-    with open(tad_filename, "r") as csvfile:
-        csvfile = open(tad_filename, "r")
-        df = pd.read_csv(csvfile, sep="\t", header=None)
-        df = df.rename(columns={0: "bin1_id", 1: "bin2_id" })
-        # Substract one from all bin1_id and bin2_id to correct for one-based indexing in OnTAD
-        df = df.sub([1, 1, 0, 0, 0], axis="columns")
-        # Use cooler anotate
-        result = cooler.annotate(df, bins)
-        result = result.drop(columns=["weight1","weight2"])
-        # Output bedpde with addtitonal columns TADlevel  TADmean  TADscore
-        result.to_csv(tad_filename[:-4]+".bedpe", header=None, index=False, sep="\t")
+    results = []
+    for tad_filename in f_list:
+        if os.stat(tad_filename).st_size != 0:
+            with open(tad_filename, "r") as csvfile:
+                csvfile = open(tad_filename, "r")
+                df = pd.read_csv(csvfile, sep="\t", header=None)
+                df = df.rename(columns={0: "bin1_id", 1: "bin2_id"})
+                # Substract one from all bin1_id and bin2_id to correct for one-based indexing in OnTAD
+                df = df.sub([1, 1, 0, 0, 0], axis="columns")
+                # Extract Chromosome name out of filename
+                region = re.findall(r"(chr\d|chr\w)", tad_filename)[0]
+                # Add offset for particular chromosome
+                offset = cooler_obj.offset(region)
+                df = df.add([offset, offset, 0, 0, 0], axis="columns")
+                # Use cooler anotate
+                result = cooler.annotate(df, bins)
+                result = result.drop(columns=["weight1", "weight2"])
+                results.append(result)
+                # Output bedpde with addtitonal columns TADlevel  TADmean  TADscore
+    bedpe = pd.concat(results)
+    bedpe.to_csv(os.path.basename(cooler_filename)[:-6] + ".binsize_" + str(binsize) + ".bedpe", header=None, index=False, sep="\t")
 
 # TODO:Stitch files together
-
-
-# Popen(['cat', 'test.py'], stdout=PIPE, stderr=PIPE)
-# uploadCommand = UPLOADTEMPLATE.format(
-#         tempFile, CLODIUSEXTENSIONS[fileType], DATATYPES[fileType], name, project, server)
-#     logging.debug(uploadCommand)
-#     # dispatch command
-#     process = subprocess.run(shlex.split(uploadCommand),
-#                              check=True, capture_output=True)
-#     if process.returncode != 0:
-#         for line in process.stderr.decode("UTF-8").split("\n"):
-#             logging.error(line)
-#             logging.error("\n\n")
-#             raise subprocess.CalledProcessError(process.returncode, uploadCommand)
-#     else:
-#         for line in process.stdout.decode("UTF-8").split("\n"):
-#             logging.debug(line)
 
 
 def create_dense_matrix(filep, binsize):
     cooler_obj = cooler.Cooler(f'{filep}::/resolutions/{binsize}')
     filename = os.path.basename(filep)
     filename_base = filename[:-6]
+    temp_folder = tempfile.mkdtemp(suffix=None, prefix=None, dir=None)
     for chr_name in chromsizes.index:
         matrix = cooler_obj.matrix(balance=True).fetch(chr_name)
         matrix = np.nan_to_num(matrix)
-        print(chr_name)
-        print("tmpdata/%s.%s.matrix" % (filename_base, chr_name))
-        np.savetxt("tmpdata/%s.%s.matrix" % (filename_base, chr_name), matrix, fmt='%1.8f', delimiter="\t")
+        # print(chr_name)
+        # print("tmpdata/%s.%s.matrix" % (temp_folder ,filename_base, chr_name))
+        np.savetxt("%s/%s.%s.matrix" % (temp_folder, filename_base, chr_name), matrix, fmt='%1.8f', delimiter="\t")
+    return temp_folder
 
 
 @click.command()
@@ -102,22 +92,27 @@ def create_dense_matrix(filep, binsize):
 # -log2 <boolean> if specified, log2(contact frequency) will be used to call TADs.
 # -o <file path> The file path for the TAD calling results.
 def main(filep, binsize, penalty, minsz, maxsz, ldiff, lsize):
-    # Creates the files in the format dfor OnTADO
-    # TODO reactivate
-    # create_dense_matrix(filep, binsize)
+    # Create Matrix for every chromosome for OnTAD
+    matrix_folder = create_dense_matrix(filep, binsize)
     # Call On TAD
-    f_list = glob.glob('tmpdata/*.matrix')
+    f_list = glob.glob('%s/*.matrix' % matrix_folder)
     processes = []
+        # Creates the files in the format for OnTADO
+    tad_folder = tempfile.mkdtemp(suffix=None, prefix=None, dir=None)
     for filename in f_list:
         # TODO parse all options
         filename_base = os.path.basename(filename)[:-7]
-        command_string = "OnTAD %s -penalty 0.1 -maxsz 200 -o out/%s" % (filename, filename_base)
-        print(command_string)
+        command_string = "OnTAD %s -penalty 0.1 -maxsz 200 -o %s/%s" % (filename, tad_folder, filename_base)
         processes.append(subprocess.Popen(command_string, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True))
     for proc in processes:
         output, error = proc.communicate()
-    #TODO add 
-    # convert_to_bedpe(cooler_filename, binsize, tad_filename):
+        if proc.returncode != 0:
+            print(error.decode("utf-8"))
+    # Creates the .bedpe wfor all chromosomes
+    convert_to_bedpe(filep, binsize, tad_folder)
+    # remove /temp dirs
+    shutil.rmtree(matrix_folder)
+    shutil.rmtree(tad_folder)
 
 
 if __name__ == "__main__":
